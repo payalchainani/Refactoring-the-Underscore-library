bb6b9f39177743f0c1398e9ca77c07e14b98a48e
Zoe Oliver <zoe@daleoliver.co.uk>
2017-08-23 11:56:12 +0100

    identity and first function complete

--- lowbar.js
+++ lowbar.js
@@ -1,7 +1,13 @@
 var _ = {};
 
-_.identity = function() {
-  // your code here 
+_.identity = function (value) {
+  return value;
 };
+
+_.first = function (list, n) {
+  if (n === undefined) return list[0];
+  return list.slice(0,n);
+};
+
 
 module.exports = _;
--- spec/lowbar.js
+++ spec/lowbar.js
@@ -13,5 +13,31 @@ describe('_', function () {
     it('is a function', function() {
       expect(_.identity).to.be.a('function');
     });
+    it('returns argument value', function () {
+      var result = _.identity('hello');
+      var expected = 'hello';
+      expect(result).to.equal(expected);
+    });
+  });
+
+  describe('#first', function () {
+    it('is a function', function() {
+      expect(_.first).to.be.a('function');
+    });
+    it('returns the first element of the given argument', function () {
+      var result = _.first ([1,2,3]);
+      var expected = 1;
+      expect(result).to.equal(expected);
+    });
+    it('returns the first element of the given argument when given second argument', function () {
+      var result = _.first ([1,2,3], 2);
+      var expected = [1,2];
+      expect(result).to.eql(expected);
+    });
+    it('works for string argument', function () {
+      var result = _.first ('hello', 2);
+      var expected = 'he';
+      expect(result).to.eql(expected);
+    });
   });
 });
